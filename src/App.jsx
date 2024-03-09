60467d1736805f97c19cd5b87e32a830a0ab4b6c
MennaAllahYasser11 <mennaallahyasser111@gmail.com>
2024-03-10 01:23:36 +0200

    upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './index.css';
+import Stopwatch from './Stopwatch.jsx';
+function App() {
+  return<>
+<h1>Stop Watch</h1>
+  <Stopwatch />
+  </>
+}
+
+export default App;
